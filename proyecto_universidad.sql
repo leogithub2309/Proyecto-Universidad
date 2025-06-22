5370303d54991c68dae126d7bac9b3250aff814b
leogithub2309 <leosep1996@gmail.com>
2025-06-22 16:12:49 -0400

    Backup de la Base de Datos

--- proyecto_universidad.sql
+++ proyecto_universidad.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-06-2025 a las 02:26:56
+-- Tiempo de generación: 22-06-2025 a las 22:11:41
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
